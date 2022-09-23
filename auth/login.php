9d879462f7e254dd755552cf7649e3b7101188a7
Emanuele <paciemanuele.2@gmail.com>
2022-09-24 01:00:33 +0200

    Aggiunto login utenti

--- auth/login.php
+++ auth/login.php
@@ -1,5 +1,10 @@
 <?php
 require_once '../utils/replacePlaceholders.php';
+require_once '../utils/dbConnection.php';
+session_start();
+if (isset($_SESSION['email']) && isset($_SESSION['password'])) {
+    header('location: ../main.php');
+}
 ?>
 
 <!DOCTYPE html>
@@ -26,10 +31,44 @@ require_once '../utils/replacePlaceholders.php';
             <fieldset>
                 <legend>Password</legend>
                 <input type="password" name="password" id="password" placeholder="Inserisci la tua password" required>
-                <img src="../images/show_password.png"  alt="show/hide password" id="show-password">
+                <img src="../images/show_password.png" alt="show/hide password" id="show-password">
             </fieldset>
             <input type="submit" value="Log in" class="btn btn-primary">
             <a href="../index.php" class="btn btn-secondary">Annulla</a>
+
+            <?php
+            if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+                if (isset($_POST['email']) && isset($_POST['password'])) {
+                    $email = mysqli_real_escape_string($link, $_POST['email']);
+                    $password  = mysqli_real_escape_string($link, $_POST['password']);
+
+                    $sql = 'SELECT password FROM users WHERE email = ?';
+                    if ($stmt = mysqli_prepare($link, $sql)) {
+                        mysqli_stmt_bind_param($stmt, 's', $email);
+                        if (mysqli_stmt_execute($stmt)) {
+                            $result = mysqli_stmt_get_result($stmt);
+                            if (mysqli_num_rows($result) == 1) {
+                                $row = mysqli_fetch_array($result);
+                                $psw = $row['password'];
+                                if (password_verify($password, $psw)) {
+                                    $_SESSION['email'] = $email;
+                                    $_SESSION['password'] = $password;
+                                    header('location: ../main.php');
+                                } else {
+                                    echo '<p class="error">ATTENZIONE: Credenziali errate </p>';
+                                }
+                            } else {
+                                echo '<p class="error">ATTENZIONE: Credenziali errate </p>';
+                            }
+                        }
+                    } else {
+                        echo '<p class="error">Errore</p>';
+                    }
+                } else {
+                    echo '<p class="error">ATTENZIONE: Compila i campi prima di proseguire</p>';
+                }
+            }
+            ?>
         </form>
     </div>
 </body>
